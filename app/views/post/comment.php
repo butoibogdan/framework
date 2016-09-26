74abf107266e4d1f98e4d849c869076a89e87a5e
butoibogdan <butoibogdan@yahoo.com>
2016-09-26 15:56:38 +0300

    Merge origin/master
    Conflicts:
    	composer.lock

--- app/views/post/comment.php
+++ app/views/post/comment.php
@@ -1,12 +1,14 @@
 <div id="left">
-    <table>
-        <?php foreach ($vars as $comentariu) { ?>
-            <tr>
-                <td><?php echo $comentariu['comments'] ?></td>
-                <td><?php echo $comentariu['created_at'] ?></td>
-            </tr>
-        <?php } ?>
-    </table>
+    <?php if (count($vars) > 0) { ?>
+        <table>
+            <?php foreach ($vars as $comentariu) { ?>
+                <tr>
+                    <td><?php echo $comentariu['comments'] ?></td>
+                    <td><?php echo $comentariu['created_at'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    <?php } ?>
     <form name="addcomment" action="" method="post">
         <textarea name="comments"></textarea><br/>
         <input type="submit" name="addcomment" value="Adauga Comentariu" />
